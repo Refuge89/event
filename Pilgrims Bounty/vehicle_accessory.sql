e80d4e2e20a6bfdb5bafe1a75421f76226f99602
Fly Man <fly.man.opensim@gmail.com>
2018-11-04 18:42:35 +0100

    Create vehicle_accessory.sql

---
+++ Pilgrims Bounty/vehicle_accessory.sql
@@ -0,0 +1,21 @@
+INSERT INTO `vehicle_accessory` (`vehicle_entry`,`accessory_entry`,`seat`, `comment`) VALUES
+	-- Bountiful Table
+	(32823,34812,0,'The Cranberry Chair'),
+	(32823,34823,1,'The Stuffing Chair'),
+	(32823,34819,2,'The Pie Chair'),
+	(32823,34824,3,'The Sweet Potato Chair'),
+	(32823,34822,4,'The Turkey Chair'),
+	(32823,32830,5,'Food Holder'),
+	(32823,32840,6,'Plate Holder'),
+	-- Food Holder
+	(32830,32824,0,'[PH] Pilgrims Bounty Table - Turkey'),
+	(32830,32827,1,'[PH] Pilgrims Bounty Table - Cranberry Sauce'),
+	(32830,32831,2,'[PH] Pilgrims Bounty Table - Stuffing'),
+	(32830,32825,3,'[PH] Pilgrims Bounty Table - Yams'),
+	(32830,32829,4,'[PH] Pilgrims Bounty Table - Pie'),
+	-- Plte Holder
+	(32840,32839,0,'Plate Holder Plate'),
+	(32840,32839,1,'Plate Holder Plate'),
+	(32840,32839,2,'Plate Holder Plate'),
+	(32840,32839,3,'Plate Holder Plate'),
+	(32840,32839,4,'Plate Holder Plate');
